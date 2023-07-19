d715b569711dc3654272e1a1fd3e674d3846c600
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-19 16:00:47 +0200

    added first bonus

--- hotels.php
+++ hotels.php
@@ -37,5 +37,40 @@ $hotels = [
         'vote' => 2,
         'distance_to_center' => 50
     ],
+    [
+        'name' => 'Hotel Bellaria',
+        'description' => 'Hotel Bellaria Descrizione',
+        'parking' => false,
+        'vote' => 3,
+        'distance_to_center' => 23
+    ],
+    [
+        'name' => 'Hotel Summer',
+        'description' => 'Hotel Summer Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 13.6
+    ],
+    [
+        'name' => 'Hotel Barbieri',
+        'description' => 'Hotel Barbieri Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 9.2
+    ],
+    [
+        'name' => 'Hotel Roma',
+        'description' => 'Hotel Roma Descrizione',
+        'parking' => false,
+        'vote' => 3,
+        'distance_to_center' => 5
+    ],
+    [
+        'name' => 'Hotel Crystal',
+        'description' => 'Hotel Crystal Descrizione',
+        'parking' => true,
+        'vote' => 5,
+        'distance_to_center' => 11
+    ],
 
 ];
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-
 include 'hotels.php';
 
+if (isset($_GET['parking'])) {
+    $filteredHotels = array_filter($hotels, function ($hotel) {
+        return $hotel['parking'] == true;
+    });
+} else {
+    $filteredHotels = $hotels;
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,13 +17,57 @@ include 'hotels.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <title>PHP | Hotels</title>
+
+    <style>
+        body {
+            color: white;
+        }
+
+        .table {
+            background-color: transparent;
+        }
+
+        .table th,
+        .table td {
+            background-color: transparent;
+            color: white;
+        }
+
+        span {
+            color: #7377AD;
+        }
+
+        footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+    </style>
+
 </head>
 
 <body class="bg-dark">
 
-    <div class="container pt-5">
-        <table class="table border-dark">
+    <header class="shadow-lg">
+        <h1 class="py-3 ps-3">Hotel<span>php</span>.com</h1>
+        <form class="py-2 ps-3 d-flex align-items-center bg-info bg-opacity-25">
+            <div class="form-check">
+                <label class="form-check-label" for="parkingCheckbox">
+                    Filtra per: parcheggio
+                </label>
+                <input class="form-check-input" type="checkbox" value="1" id="parkingCheckbox" name="parking">
+            </div>
+            <button type="submit" class="btn btn-primary ms-3">Applica filtro</button>
+        </form>
+    </header>
+
+
+    <main class="container pt-4">
+
+        <table class="table border-light">
             <thead>
                 <tr>
                     <th>Name</th>
@@ -28,18 +78,30 @@ include 'hotels.php';
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($hotels as $hotel) : ?>
+                <?php foreach ($filteredHotels as $hotel) : ?>
                     <tr>
                         <td><?php echo $hotel['name']; ?></td>
                         <td><?php echo $hotel['description']; ?></td>
-                        <td><?php echo $hotel['parking'] ? 'True' : 'False'; ?></td>
-                        <td><?php echo $hotel['vote']; ?></td>
+                        <td class="ps-4">
+                            <?php if ($hotel['parking']) : ?>
+                                <i class="bi bi-check-circle-fill text-success ps-1"></i>
+                            <?php else : ?>
+                                <i class="bi bi-x-circle-fill text-danger ps-1"></i>
+                            <?php endif; ?>
+                        </td>
+                        <td><?php echo $hotel['vote']; ?>/5</td>
                         <td><?php echo $hotel['distance_to_center']; ?> km</td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
-    </div>
+    </main>
+
+    <footer class="text-center bg-dark">
+
+        <p>powered by <span>php</span></p>
+
+    </footer>
 
 </body>
 
