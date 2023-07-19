2bd3e06bb6f827c1b5e77eff16505a7b3d6e4615
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-19 09:20:28 +0200

    inserted data into bs table

--- index.php
+++ index.php
@@ -10,22 +10,36 @@ include 'hotels.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>PHP | Hotels</title>
 </head>
 
-<body>
+<body class="bg-dark">
 
-    <ul>
-        <?php foreach ($hotels as $hotel) : ?>
-            <li>
-                <h3><?php echo $hotel['name']; ?></h3>
-                <p><?php echo $hotel['description']; ?></p>
-                <p>Parking: <?php echo $hotel['parking'] ? 'True' : 'False'; ?></p>
-                <p>Vote: <?php echo $hotel['vote']; ?></p>
-                <p>Distance to Center: <?php echo $hotel['distance_to_center']; ?> km</p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="container pt-5">
+        <table class="table border-dark">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Parking</th>
+                    <th>Vote</th>
+                    <th>Distance to Center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr>
+                        <td><?php echo $hotel['name']; ?></td>
+                        <td><?php echo $hotel['description']; ?></td>
+                        <td><?php echo $hotel['parking'] ? 'True' : 'False'; ?></td>
+                        <td><?php echo $hotel['vote']; ?></td>
+                        <td><?php echo $hotel['distance_to_center']; ?> km</td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 
 </body>
 
